474888b69e7cab156b677da1515e91e6971cca61
Jorge Rodriguez <jorge.rodriguez@marandu.com.ar>
2026-07-14 23:23:36 -0300

    Agrego Sourcing Handler -  EventSourcing

---
+++ Projects/Ticketing.Command/Domain/Abstract/IEventSourcingHandler.cs
@@ -0,0 +1,7 @@
+﻿namespace Ticketing.Command.Domain.Abstract;
+
+public interface IEventSourcingHandler<T>
+{
+    Task<T> GetByIdAsync(string aggregateId, CancellationToken cancellationToken);
+    Task SaveChanges(AggregateRoot aggregate, CancellationToken cancellationToken);
+}
---
+++ Projects/Ticketing.Command/Infrastructure/EventSourcings/TicketingEventSourcingHandler.cs
@@ -0,0 +1,18 @@
+﻿using Ticketing.Command.Application.Aggregates;
+using Ticketing.Command.Domain.Abstract;
+
+namespace Ticketing.Command.Infrastructure.EventSourcings
+{
+    public class TicketingEventSourcingHandler : IEventSourcingHandler<TicketAggregate>
+    {
+        public Task<TicketAggregate> GetByIdAsync(string aggregateId, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveChanges(AggregateRoot aggregate, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
