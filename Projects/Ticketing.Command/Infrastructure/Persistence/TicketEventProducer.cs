80a0bcd6b80aaefdb75dd2665e1dcfeead505dfe
Jorge Rodriguez <jorge.rodriguez@marandu.com.ar>
2026-07-20 20:18:07 -0300

    Agrego logica de producer para Apache Kafka

--- Projects/Ticketing.Command/Infrastructure/Persistence/TicketEventProducer.cs
+++ Projects/Ticketing.Command/Infrastructure/Persistence/TicketEventProducer.cs
@@ -1,5 +1,8 @@
 ﻿using Common.Core.Events;
 using Common.Core.Producers;
+using Confluent.Kafka;
+using Microsoft.Extensions.Options;
+using System.Text.Json;
 using Ticketing.Command.Application.Models;
 
 namespace Ticketing.Command.Infrastructure.Persistence;
@@ -8,13 +11,34 @@ public class TicketEventProducer : IEventProducer
 {
     private readonly KafkaSettings _kafkaSettings;
 
-    public TicketEventProducer(KafkaSettings kafkaSettings)
+    public TicketEventProducer(IOptions<KafkaSettings> kafkaSettings)
     {
-        _kafkaSettings = kafkaSettings;
+        _kafkaSettings = kafkaSettings.Value;
     }
 
-    public Task ProduceAsync(string topic, BaseEvent @event)
+    public async Task ProduceAsync(string topic, BaseEvent @event)
     {
-        throw new NotImplementedException();
+        var config = new ProducerConfig
+        {
+            BootstrapServers = $"{_kafkaSettings.Hostname}:{_kafkaSettings.Port}"
+        };        
+
+        using var producer = new ProducerBuilder<string, string>(config)
+            .SetKeySerializer(Serializers.Utf8)
+            .SetValueSerializer(Serializers.Utf8)
+            .Build();
+
+        var eventMessage = new Message<string, string>
+        {
+            Key = Guid.NewGuid().ToString(), // o usa una propiedad del evento si existe
+            Value = JsonSerializer.Serialize(@event)
+        };
+
+        var deliveryStatus = await producer.ProduceAsync(topic, eventMessage);
+        if (deliveryStatus.Status == PersistenceStatus.NotPersisted)
+        {
+            throw new ArgumentException($"Failed to produce event to topic {topic}, of message {@event.GetType().Name}, por la siguiente razon, {deliveryStatus.Message}");
+        }
+        //producer.Flush(TimeSpan.FromSeconds(10));
     }
 }
