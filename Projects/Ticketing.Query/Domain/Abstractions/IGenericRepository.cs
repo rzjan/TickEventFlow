aed17608fc5193cc4bb2310c9a543b6e2311fb99
Jorge Rodriguez <jorge.rodriguez@marandu.com.ar>
2026-07-21 20:30:09 -0300

    Genero generic repository

---
+++ Projects/Ticketing.Query/Domain/Abstractions/IGenericRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Ticketing.Query.Domain.Abstractions;
+
+public interface IGenericRepository<T> where T: class
+{
+    Task<IReadOnlyList<T>> GetAllAsync();
+    Task<T?> GetByAdAsync(Guid id);
+    void AddEntity(T Entity);
+    void UpdateEntity(T Entity);
+    void DeleteEntity(Guid id);
+}
---
+++ Projects/Ticketing.Query/Domain/Abstractions/IUnitOfWork.cs
@@ -0,0 +1,7 @@
+﻿namespace Ticketing.Query.Domain.Abstractions;
+
+public interface IUnitOfWork
+{
+    IGenericRepository<T> RepositoryGeneric<T>() where T : class;
+    Task<int> Complete();
+}
