d0dc86b8bb9dda1bbbe54a99144cd317ebb15718
Jorge Rodriguez <jorge.rodriguez@marandu.com.ar>
2026-07-21 15:50:26 -0300

    Agrego DbContextFactory para proceso backround

---
+++ Projects/Ticketing.Query/Infrastructure/Persistence/DataBaseContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ticketing.Query.Infrastructure.Persistence;
+
+public class DataBaseContextFactory
+{
+    private readonly Action<DbContextOptionsBuilder> _configureDbContext;
+
+    public DataBaseContextFactory(Action<DbContextOptionsBuilder> configureDbContext)
+    {
+        _configureDbContext = configureDbContext;
+    }
+
+    public TicketDbContext CreateDbContext()
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<TicketDbContext>();
+        _configureDbContext(optionsBuilder);
+        return new TicketDbContext(optionsBuilder.Options);
+    }
+}
---
+++ Projects/Ticketing.Query/Infrastructure/Persistence/TicketDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ticketing.Query.Domain.Employees;
+using Ticketing.Query.Domain.Tickets;
+
+namespace Ticketing.Query.Infrastructure.Persistence;
+
+public class TicketDbContext : DbContext
+{
+    public TicketDbContext(DbContextOptions<TicketDbContext> options):base(options)
+    {        
+    }
+
+    public virtual DbSet<Ticket> Tickets { get; set; } = null!;
+    public virtual DbSet<Employee> Employees { get; set; } = null!;
+
+    override protected void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(TicketDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);        
+    }
+}
