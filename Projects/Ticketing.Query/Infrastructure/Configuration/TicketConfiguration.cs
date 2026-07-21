988de5e41cb71733848554c10b786a5d2cf2189e
Jorge Rodriguez <jorge.rodriguez@marandu.com.ar>
2026-07-21 15:34:25 -0300

    Agrego entity configuration en EF

--- Projects/Ticketing.Query/Infrastructure/Configuration/TicketConfiguration.cs
+++ Projects/Ticketing.Query/Infrastructure/Configuration/TicketConfiguration.cs
@@ -5,6 +5,14 @@ using Ticketing.Query.Domain.TicketTypes;
 
 namespace Ticketing.Query.Infrastructure.Configuration;
 
+public class TicketEmployeeConfiguration : IEntityTypeConfiguration<TicketEmployee>
+{
+    public void Configure(EntityTypeBuilder<TicketEmployee> builder)
+    {
+        builder.ToTable("TicketEmployees");
+        builder.HasKey(te => new { te.TicketId, te.EmploteeId });
+    }
+}
 public class TicketConfiguration : IEntityTypeConfiguration<Ticket>
 {
     public void Configure(EntityTypeBuilder<Ticket> builder)
@@ -15,6 +23,29 @@ public class TicketConfiguration : IEntityTypeConfiguration<Ticket>
             .HasConversion(
                 v => v!.Id,
                 v => TicketType.Create(v)
+            );
+
+        //relacion muchos a muchos entre Ticket y Employee
+        //a traves de la tabla intermedia TicketEmployee
+        builder
+            .HasMany(e => e.Employees)
+            .WithMany(t => t.Tickets)
+            //Se crea la tabla intermedia TicketEmployee
+            .UsingEntity<TicketEmployee>(
+                //Se configura la relación entre TicketEmployee y Employee
+                j => j
+                .HasOne(p => p.Employee)
+                .WithMany(p => p.TicketEmployees)
+                .HasForeignKey(p => p.EmploteeId),
+                //Se configura la relación entre TicketEmployee y Ticket
+                j => j
+                .HasOne(p => p.Ticket)
+                .WithMany(p => p.TicketEmployees)
+                .HasForeignKey(p => p.TicketId),
+                //Clave primaria compuesta
+                j => { 
+                  j.HasKey(t => new { t.TicketId, t.EmploteeId });
+                }
             );
     }
 }
---
+++ Projects/Ticketing.Query/Infrastructure/Configuration/TicketTypeConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Common.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ticketing.Query.Domain.TicketTypes;
+
+namespace Ticketing.Query.Infrastructure.Configuration;
+
+public class TicketTypeConfiguration : IEntityTypeConfiguration<TicketType>
+{
+    public void Configure(EntityTypeBuilder<TicketType> builder)
+    {
+        builder.ToTable("TicketTypes");
+        builder.HasKey(t => t.Id);
+        
+        IEnumerable<TicketType> ticketTypes = Enum
+                                                .GetValues<TicketTypeEnum>()
+                                                //.Cast<TicketTypeEnum>()
+                                                .Select(e => TicketType.Create((int)e));
+
+        builder.HasData(ticketTypes);
+    }
+}
