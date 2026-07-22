c679a2260cdccbcf520520223f2b267857d304a3
Jorge Rodriguez <jorge.rodriguez@marandu.com.ar>
2026-07-21 21:53:32 -0300

    Agrego deserializer objects consumer y TicketUpdatedEvent

---
+++ Common/Common.Core/Events/TicketUpdatedEvent.cs
@@ -0,0 +1,14 @@
+﻿namespace Common.Core.Events;
+
+public class TicketUpdatedEvent: BaseEvent
+{
+    public TicketUpdatedEvent(string type) : base(type)
+    {
+    }
+
+    public string? Status { get; set; }
+    public string? Description { get; set; }
+    public string? Username { get; set; }
+
+
+}
---
+++ Projects/Ticketing.Query/Infrastructure/Converters/EventJsonConverter.cs
@@ -0,0 +1,41 @@
+﻿using Common.Core.Events;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Ticketing.Query.Infrastructure.Converters;
+
+public class EventJsonConverter : JsonConverter<BaseEvent>
+{
+
+
+    //public override bool CanConvert(Type type)
+    //{
+    //    return type.IsAssignableFrom(typeof(BaseEvent));
+    //}
+
+    public override BaseEvent? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (!JsonDocument.TryParseValue(ref reader, out var doc))
+        {
+            throw new JsonException($"Failed to parse JSON document {nameof(JsonDocument)}.");
+        }
+        if (!doc.RootElement.TryGetProperty("Type", out var type))
+        {
+            throw new JsonException($"Missing property 'Type' in JSON document.");
+        }
+        var typeDiscriminator = type.GetString();
+        var json = doc.RootElement.GetRawText();
+
+        return typeDiscriminator switch
+        {
+            nameof(TicketCreatedEvent) => JsonSerializer.Deserialize<TicketCreatedEvent>(json, options),
+            nameof(TicketUpdatedEvent) => JsonSerializer.Deserialize<TicketUpdatedEvent>(json, options),
+            _ => throw new JsonException($"Unknown event type '{typeDiscriminator}'.")
+        };
+    }
+
+    public override void Write(Utf8JsonWriter writer, BaseEvent value, JsonSerializerOptions options)
+    {
+        throw new NotImplementedException();
+    }
+}
